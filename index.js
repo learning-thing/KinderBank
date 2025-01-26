8591463a2cc4d25b62760405207a602842041608
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2025-01-26 16:47:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let banana = false;
+let i = 0;
+
+let title = document.getElementById("timmy");
+
+function hello() {
+    console.log("Hello world"+i);
+    title.innerHTML += "\n"+i
+    i+=1;
+}
+
+function neuesKind() {
+    
+}
